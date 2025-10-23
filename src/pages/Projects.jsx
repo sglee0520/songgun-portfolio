6d03e450f5040a5dfe93ce4c6f00ab7aaa07c7c2
sglee0520 <95568380+sglee0520@users.noreply.github.com>
2025-10-22 22:51:04 -0700

    Create Projects.jsx

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+function Projects() {
+  return (
+    <div className="page-container">
+      <h1>My Projects</h1>
+      <div className="projects-grid">
+        {/* Project cards will be added here with grid layout */}
+        <p>Project showcase coming soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
