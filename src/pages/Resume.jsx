c28ff9fa04088b5ee2f673b4b12ac40073b59440
sglee0520 <95568380+sglee0520@users.noreply.github.com>
2025-10-22 22:53:03 -0700

    Create Resume.jsx

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,13 @@
+function Resume() {
+  return (
+    <div className="page-container">
+      <h1>Resume</h1>
+      <div className="resume-content">
+        {/* Professional experience and education will be added here */}
+        <p>Professional experience and education details coming soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Resume
