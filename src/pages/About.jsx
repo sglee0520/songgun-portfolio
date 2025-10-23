aa9529fdbf6b498268205b164aac0967099cccc4
sglee0520 <95568380+sglee0520@users.noreply.github.com>
2025-10-22 22:50:08 -0700

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div className="page-container">
+      <h1>About Me</h1>
+      <p>Personal background and skills information will be added here.</p>
+    </div>
+  )
+}
+
+export default About
