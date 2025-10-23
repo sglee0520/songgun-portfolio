0ca1466081c0b29cc71f4f7c20404708dcc5a5f7
sglee0520 <95568380+sglee0520@users.noreply.github.com>
2025-10-22 22:51:57 -0700

    Create Writing.jsx

---
+++ src/pages/Writing.jsx
@@ -0,0 +1,13 @@
+function Writing() {
+  return (
+    <div className="page-container">
+      <h1>My Writing</h1>
+      <div className="writing-list">
+        {/* Blog posts and articles will be added here */}
+        <p>Articles and blog posts coming soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Writing
