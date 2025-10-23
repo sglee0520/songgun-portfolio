009905d11c9fc1fc456dd820f1527abf130ad8bb
sglee0520 <95568380+sglee0520@users.noreply.github.com>
2025-10-22 22:54:23 -0700

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/writing">Writing</Link></li>
+        <li><Link to="/resume">Resume</Link></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
